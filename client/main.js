b0303a3588f542701c334a34de13474c515d132a
Hardik <hardikvala24@gmail.com>
2023-03-14 23:04:36 -0700

    Call HackerNews API from server and view through client (#4)

--- client/main.js
+++ client/main.js
@@ -1,23 +1,63 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form');
+const outputContainer = document.querySelector('#output_container');
 
-setupCounter(document.querySelector('#counter'))
+function getServerUrl() {
+  if (import.meta.env.PROD) {
+    return import.meta.env.VITE_APP_SERVER_URL_PROD;
+  }
+  return import.meta.env.VITE_APP_SERVER_URL_DEV;
+}
+
+// copied from https://dmitripavlutin.com/timeout-fetch-request/
+async function fetchWithTimeout(resource, options = {}) {
+  // 8 secs
+  const { timeout = 8000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+  return response;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  const serverUrl = getServerUrl();
+  console.log(`Server URL: ${serverUrl}`);
+
+  form.reset();
+
+  try {
+    const response = await fetchWithTimeout(serverUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        storyId: data.get('storyId')
+      }),
+      // 30 sec
+      timeout: 30000,
+    });
+    
+    if (response.ok) {
+      // const data = await response.json();
+      const data = await response.text();
+      outputContainer.innerHTML = data;
+    } else {
+      const err = await response.text();
+      alert(`code: ${response.status}\nerror: ${response.statusText}`);
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
+
+form.addEventListener('submit', handleSubmit);
--- server/server.js
+++ server/server.js
@@ -18,10 +18,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// copied from https://dmitripavlutin.com/timeout-fetch-request/
+async function fetchWithTimeout(resource, options = {}) {
+    // 8 secs
+    const { timeout = 8000 } = options;
+    
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal  
+    });
+    clearTimeout(id);
+    return response;
+}
+
 app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Server is running.',
-    })
+    });
 });
+
+app.post('/', async (req, res) => {
+    console.log({ req });
+
+    try {
+        // e.g. 35162458
+        const storyId = req.body.storyId;
+
+        const apiUrl = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`;
+        const response = await fetchWithTimeout(apiUrl)
+            .then(response => response.json());
+
+        res.status(200).send({
+            message: response,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+})
 
 app.listen(5001, () => { console.log('Server is running.') });
